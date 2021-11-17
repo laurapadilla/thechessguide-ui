94c364bdeb6b6950b001dc967deb79740023db9f
Laura Padilla <hi@lauracodes.com>
2021-11-17 11:06:16 -0500

    Hookup backend to frontend

--- pages/opening/[slug].js
+++ pages/opening/[slug].js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import { sanityClient } from "../../sanity";
 
-const Opening = () => {
-  return <h1>Opening</h1>;
+const Opening = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const pageSlug = pageContext.query.slug;
+
+  const query = `*[ _type == "opening" && slug.current == $pageSlug][0] {
+    name, id, piece, description, moves, resources[]{...}
+  }`;
+  const opening = await sanityClient.fetch(query, { pageSlug });
+
+  if (!opening) {
+    return {
+      props: null,
+      notFound: true,
+    };
+  } else {
+    return {
+      props: {
+        name: opening.name,
+        id: opening.id,
+        description: opening.description,
+        piece: opening.piece,
+        moves: opening.moves,
+        resources: opening.resources,
+      },
+    };
+  }
 };
 
 export default Opening;
