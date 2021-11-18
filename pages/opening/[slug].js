cda275eabc67bfacf3231b7dc9b493b81e847b34
Laura Padilla <hi@lauracodes.com>
2021-11-18 09:49:50 -0500

    Start to style the data

--- pages/opening/[slug].js
+++ pages/opening/[slug].js
@@ -1,8 +1,45 @@
 import React from "react";
 import { sanityClient } from "../../sanity";
+import styled from "styled-components";
 
-const Opening = ({ name }) => {
-  return <h1>{name}</h1>;
+const Container = styled.main`
+  background-color: aliceblue;
+  padding: 4rem;
+`;
+
+const OpeningName = styled.h1`
+  font-size: 2.25rem;
+`;
+const Moves = styled.h4`
+  font-size: 2rem;
+  font-weight: normal;
+`;
+
+export const Sup = styled.sup`
+  font-size: 1.5rem;
+  vertical-align: top;
+  margin-right: 1rem;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+  color: gray;
+`;
+
+const Opening = ({ name, id, description, piece, moves, resources }) => {
+  return (
+    <Container>
+      <article>
+        <OpeningName>
+          <Sup>{id}</Sup>
+          {name}
+        </OpeningName>
+      </article>
+      <p>{piece}</p>
+      <Moves>{moves}</Moves>
+      <Description>{description}</Description>
+    </Container>
+  );
 };
 
 export const getServerSideProps = async (pageContext) => {
